110af470b3a0431f2c419c0eea86fa5a1afd270f
6895mahfuz@gmail.com <6895mahfuz@gmail.com>
2017-05-06 14:25:51 +0600

    added bootstrap table in index page

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
-<?php
-echo "index";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<title</title>
+</head>
+
+<body>
+      <div class="container">
+          <div class="row">
+            <center><h1>To do list</h1></center>
+
+    <div class="col-md-10 col-md-offset">
+              <table class="table">
+                <button class="btn btn-success" >Add Task</button>
+                <button class="btn btn-default pull-right" >Print</button>
+                <hr><br>
+<thead>
+  <tr>
+    <th>N0</th>
+    <th>Task</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <th scope="row">1</th>
+    <td>Mark</td>
+  </tr>
+</tbody>
+</table>
+            </div>
+
+
+          </div>
+
+      </div>
+</body>
+</html>
